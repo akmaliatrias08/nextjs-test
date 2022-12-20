1741852a2eab7057121186b3ef913df1dc30ac65
Hasta Ragil Saputra <hasta.ragil@bangun-kreatif.com>
2022-12-20 17:42:15 +0700

    feat: sample SWR

--- app/(authenticated)/home/page.tsx
+++ app/(authenticated)/home/page.tsx
@@ -4,11 +4,18 @@ import React from "react";
 import {Button, Card} from "antd";
 import {store} from "#/store";
 import {observer} from "mobx-react-lite";
+import {sampleRepository} from "#/repository/sample";
 
 const Page = observer(() => {
+    const { data, error, isLoading }  = sampleRepository.hooks.useJoke();
     return <div>
-        home: {store.ui.title}
-        <Button onClick={() => {
+        <div>
+            home: {store.ui.title}
+        </div>
+        <div>
+            fact: {data?.setup}
+        </div>
+        <Button className={"ml-8"} onClick={() => {
             store.ui.changeTitle("from home")
         }}>change title</Button>
     </div>;
