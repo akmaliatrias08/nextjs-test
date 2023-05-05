e936cb2eca53a21b98d3564dab3a170e631b4473
ahmadzuhdi <ahmad.zuhdi.y@gmail.com>
2023-05-05 16:41:56 +0700

    feat: dynamic env

--- app/head.tsx
+++ app/head.tsx
@@ -12,6 +12,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Boilerplate" />
       <link rel="icon" href="/favicon.ico" />
+      <script src="/api/env" async></script>
     </>
   )
 }
